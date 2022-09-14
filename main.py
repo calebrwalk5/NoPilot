0ebbf0a9d69100f9f947ec422207ef44ffe63c4b
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-09-14 12:17:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from NoPilot import GPT2
 
 def run():
     gpt2 = GPT2()
-
+    line = 1
     print("Reading Code...")
     while True:
         try:
-            text = input("1| ")
+            text = input(line " | ")
             prediction = gpt2.predict_next(text, 1)
             print("{} {}".format(text, prediction))
-
+            line += 1
+            
         except KeyError:
             pass
 
